c15c21b76d04571a0798cab0f50d371f6496b04c
mahmoud-khader <mahmoudkhader2010@gmail.com>
2021-09-26 16:30:59 +0300

    lab31 done

---
+++ src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Navbar, Button, Alignment } from "@blueprintjs/core";
+
+export default function Header() {
+    return (
+        <>
+        <Navbar className="header" style={{backgroundColor:"#c64756"}}>
+        <Navbar.Group align={Alignment.LEFT}>
+            <Navbar.Heading className="title"><h2>To-Do📋</h2></Navbar.Heading>
+            <Navbar.Divider />
+            <Button className="bp4-minimal" icon="home" text="Home" />
+        </Navbar.Group>
+    </Navbar>
+    </>
+    );
+  }
