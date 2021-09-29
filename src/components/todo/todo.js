171e369e045e0a94a7613e7c7afa6167e70bee31
mahmoud-khader <mahmoudkhader2010@gmail.com>
2021-09-29 21:03:39 +0300

    lab34 done

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -5,34 +5,94 @@ import List from "../list/List";
 import "./todo.css";
 import Form from "../form/Form.js";
 import { SettingsContext } from "../../context/context.js";
+import superagent from "superagent";
+import { LoginContext } from "../../context/loginContext.js";
+
 const ToDo = () => {
+  const API = "https://todo-backend-5.herokuapp.com"
   const settings = useContext(SettingsContext);
+  const loginContext=useContext(LoginContext)
+
   const [list, setList] = useState([]);
   const [incomplete, setIncomplete] = useState([]);
   const { handleChange, handleSubmit } = useForm(addItem);
 
-  function addItem(item) {
-    console.log(item);
-    item.id = uuid();
+  useEffect( async () => {
+    try {
+      const res = await superagent.get(`${API}/api/v1/todo`)
+      .set('Authorization', 'Bearer ' + loginContext.token)
+        setList(res.body)
+        console.log(res);
+
+  } catch (error) {
+      alert('Invalid Render');
+  }
+
+  }, [loginContext.isUpdated,settings.showCompleted, settings.itemsPerPage]);
+
+  async function addItem(item) {
+    console.log(list);
+    // item.id = uuid();
     item.complete = false;
-    setList([...list, item]);
-  }
+    
 
-  function deleteItem(id) {
-    const items = list.filter((item) => item.id !== id);
-    setList(items);
-  }
+      console.log(loginContext.token);
 
-  function toggleComplete(id) {
-    const items = list.map((item) => {
-      if (item.id == id) {
-        item.complete = !item.complete;
+      let obj = {
+        toDoItem: item.text,
+        assignedTo:item.assignee,
+        difficulty:item.difficulty,
+        complete:item.complete,
       }
-      return item;
-    });
-
-    setList(items);
+    try {
+      console.log(item.text);
+      const res = await superagent.post(`${API}/api/v1/todo`)
+      .send(obj)
+      .set('Authorization', 'Bearer ' + loginContext.token)
+      console.log(res);
+      console.log(list);
+  } catch (error) {
+      alert('Invalid data');
   }
+setList([...list, item]);
+  }
+
+  async function deleteItem(id) {
+    console.log(id);
+    try {
+      const res = await superagent.delete(`${API}/api/v1/todo/${id}`)
+      .set('Authorization', 'Bearer ' + loginContext.token)
+      const items = list.filter(item => item.id !== id);
+      setList(items);
+      console.log("items>>>>",items);
+      console.log("delete>>>>>",res);
+
+  } catch (error) {
+      alert('Invalid delete');
+  }
+
+  }
+
+  async function toggleComplete(id) {
+    console.log(id);
+    let updatedItem;
+  const items = list.map(item => {
+    if (item.id == id) {
+      item.complete = !item.complete;
+      updatedItem=item;
+    }
+    return item;
+  });
+  
+ 
+    const res = await superagent.put(`${API}/api/v1/todo/${id}`)
+    .send(updatedItem)
+    .set('Authorization', 'Bearer ' + loginContext.token)
+    loginContext.setIsUpdated(!loginContext.isUpdated);
+    
+    // setList([...list, updatedItem]);
+}
+
 
   useEffect(() => {
     let incompleteCount = list.filter((item) => !item.complete);
